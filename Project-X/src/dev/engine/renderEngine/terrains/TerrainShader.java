c6cecdac2976ae673132f787b977d7be11c46082
Baris Doga Yavas <barisdogayavas@gmail.com>
2018-07-03 13:57:48 +0300

    Improvement
    Now we are loading maps with xml files. Also multiple lights, point lights, and terrain multitexturing alpha channel implemented.
    
    0000004

--- Project-X/src/dev/engine/renderEngine/terrains/TerrainShader.java
+++ Project-X/src/dev/engine/renderEngine/terrains/TerrainShader.java
@@ -30,6 +30,7 @@ public class TerrainShader extends ShaderProgram{
 	private int location_rTexture;
 	private int location_gTexture;
 	private int location_bTexture;
+	private int location_aTexture;
 	private int location_blendMapTexture;
 	private int location_densityOfFog;
 	private int location_gradientOfFog;
@@ -60,6 +61,7 @@ public class TerrainShader extends ShaderProgram{
 		this.location_rTexture = super.getUniformLocation("rTexture");
 		this.location_gTexture = super.getUniformLocation("gTexture");
 		this.location_bTexture = super.getUniformLocation("bTexture");
+		this.location_aTexture = super.getUniformLocation("aTexture");
 		this.location_blendMapTexture = super.getUniformLocation("blendMapTexture");
 		this.location_densityOfFog = super.getUniformLocation("densityOfFog");
 		this.location_gradientOfFog = super.getUniformLocation("gradientOfFog");
@@ -118,7 +120,8 @@ public class TerrainShader extends ShaderProgram{
 		super.loadInt(location_rTexture, 1);
 		super.loadInt(location_gTexture, 2);
 		super.loadInt(location_bTexture, 3);
-		super.loadInt(location_blendMapTexture, 4);
+		super.loadInt(location_aTexture, 4);
+		super.loadInt(location_blendMapTexture, 5);
 	}
 	
 	public void loadFogVariables(float densityOfFog, float gradientOfFog) {
