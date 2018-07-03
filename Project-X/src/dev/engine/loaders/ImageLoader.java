0db591379b753561800280c1da05effe66b7d29c
beremaran <beremaran@gmail.com>
2018-07-03 21:14:35 +0300

    created methods to load cube map and texture files for skybox
    loadCubeMap() method allows to load texture files to a GL texture as
    cube maps.
    
    decodeTextureFile() method decodes PNG images and loads it to
    TextureData class

--- Project-X/src/dev/engine/loaders/ImageLoader.java
+++ Project-X/src/dev/engine/loaders/ImageLoader.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,18 +12,22 @@ import javax.imageio.ImageIO;
 
 import org.lwjgl.opengl.EXTTextureFilterAnisotropic;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL14;
 import org.lwjgl.opengl.GL30;
 import org.lwjgl.opengl.GLContext;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 
+import de.matthiasmann.twl.utils.PNGDecoder;
+import de.matthiasmann.twl.utils.PNGDecoder.Format;
 import dev.engine.EngineConfig;
+import dev.engine.skybox.TextureData;
 
 public class ImageLoader {
-	
+
 	private static List<Integer> allTextures = new ArrayList<Integer>();
-	
+
 	public static int loadTexture(String pngFileName) {
 		Texture texture = null;
 		try {
@@ -30,10 +35,11 @@ public class ImageLoader {
 			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
 			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
 			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, EngineConfig.MIPMAPPING_LEVEL);
-			if(GLContext.getCapabilities().GL_EXT_texture_filter_anisotropic) {
+			if (GLContext.getCapabilities().GL_EXT_texture_filter_anisotropic) {
 				float amount = Math.min(4f, GL11.glGetFloat(EXTTextureFilterAnisotropic.GL_TEXTURE_MAX_ANISOTROPY_EXT));
-				GL11.glTexParameterf(GL11.GL_TEXTURE_2D, EXTTextureFilterAnisotropic.GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, amount);
-			}else {
+				GL11.glTexParameterf(GL11.GL_TEXTURE_2D, EXTTextureFilterAnisotropic.GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT,
+						amount);
+			} else {
 				System.err.println("Anisotropic Filering Doesn't Supported!");
 			}
 		} catch (Exception e) {
@@ -42,30 +48,75 @@ public class ImageLoader {
 			System.exit(-1);
 		}
 		allTextures.add(texture.getTextureID());
-		
+
 		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
 		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
 		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT);
 		GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT);
 		return texture.getTextureID();
 	}
-	
-	public static BufferedImage loadBufferedImage(String file){
-		BufferedImage bufferedImage = null;
+
+	public static int loadCubeMap(String[] textureFiles) {
+		int texID = GL11.glGenTextures();
+		allTextures.add(texID);
+
+		GL13.glActiveTexture(texID);
+		GL11.glBindTexture(GL13.GL_TEXTURE_CUBE_MAP, texID);
+
+		for (int i = 0; i < textureFiles.length; i++) {
+			TextureData data = decodeTextureFile("res/skybox/" + textureFiles[i] + ".png");
+
+			GL11.glTexImage2D(GL13.GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL11.GL_RGBA, data.getWidth(),
+					data.getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, data.getBuffer());
+		}
 		
+		GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_LINEAR);
+		GL11.glTexParameteri(GL13.GL_TEXTURE_CUBE_MAP, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
+		
+		return texID;
+	}
+
+	private static TextureData decodeTextureFile(String fileName) {
+		int width = 0;
+		int height = 0;
+		ByteBuffer buffer = null;
+
+		try {
+
+			FileInputStream in = new FileInputStream(fileName);
+			PNGDecoder decoder = new PNGDecoder(in);
+			width = decoder.getWidth();
+			height = decoder.getHeight();
+			buffer = ByteBuffer.allocateDirect(4 * width * height);
+			decoder.decode(buffer, width * 4, Format.RGBA);
+			buffer.flip();
+			in.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Tried to load texture " + fileName + ", unsuccessful.");
+			System.exit(-1);
+		}
+
+		return new TextureData(buffer, width, height);
+	}
+
+	public static BufferedImage loadBufferedImage(String file) {
+		BufferedImage bufferedImage = null;
+
 		try {
 			bufferedImage = ImageIO.read(new File("res/" + file + ".png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return bufferedImage;
 	}
-	
+
 	public static void cleanUp() {
-		for(int id : allTextures){
+		for (int id : allTextures) {
 			GL11.glDeleteTextures(id);
 		}
 	}
-	
+
 }
